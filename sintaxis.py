ffd0bb6ccb5004e528ef2ce2268faefdcae604a5
Josué Adrián Rivera Cruz <145291448+josu460@users.noreply.github.com>
2024-01-25 16:37:18 -0600

    sintaxis  python

--- sintaxis.py
+++ sintaxis.py
@@ -9,3 +9,69 @@ soy un comentario
 de varias lineas
 
 '''
+
+# 2. cadenas 
+
+# print('soy una cadena')
+# print("soy una cadena")
+
+# salto de linea es \n
+
+# a= 'mi banda  \n favorita es '
+# b= " grupo marrano"
+
+# print(a+b)
+
+# print(a,b)
+
+# #contar caracteres 
+# print(len(a))
+
+# print(b[2:5])
+# print(b[:5])
+# print(b[2:])
+
+#3 Variables-- nunguna debe empezar con numeros 
+
+# x= int(3)
+# y= str("3")
+# z= float(3.2) 
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+# #Numeros aleatorios
+# import random
+# numero= random.randrange(1,1500)
+# print(numero)
+
+
+#4. Solicitud de datos 
+
+# var1= input("introduce cualquier dato")
+# var2= str(input("introduce cadenas"))
+# var3= int(input("introduce solo enteros"))
+# var4= float(input("introduce numeros decimales"))
+
+# print(var1,var2,var3,var4)
+
+
+#5. Booleans, Operadores de comparacion y logicos 
+
+"""print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+print(10 >= 9)
+print(10 != 9)
+print(10 <= 9)"""
+
+#Y estos los logicos
+# x= 1
+# print(x < 5 and x< 10)
+# print(x < 5 or x< 10)
+# print(not(x < 5 and x< 10))
+
+#estos sirven para operaciones binarias 
+# print(x < 5 & x < 10)
+# print(x < 5 | x < 10)
+# print(not(x < 5 & x < 10))
