0bd54c87b0946969af834705bbfc00461c9999d3
Josué Adrián Rivera Cruz <145291448+josu460@users.noreply.github.com>
2024-02-12 20:29:51 -0600

    avance ejercicios listas y tuplas

---
+++ listas_tuplas/ejercicio2.py
@@ -0,0 +1,8 @@
+# Crea un programa que llene una lista de 30 valores aleatorios en un rango del 10
+# al 20, con la lista llena el usuario podrá
+# a. Contar número repetidos
+# b. Eliminar numero repetidos
+# c. Remplazar los repetidos con 0
+
+import random
+print(random.randrange(10,20))
---
+++ listas_tuplas/listas.py
@@ -0,0 +1,31 @@
+#listas y tuplas
+#listas=[]
+#tuplas=()
+
+# ejercicio 1:Crea un programa usando funciones y lo que necesites para que el usuario inserte
+# N números en una Tupla, después de la captura debe desplegar el siguiente menú
+# funcional
+# 1. Sumatoria de los elementos de la lista
+# 2. Numero mayor de la lista  //ya esta
+# 3. Numero menor de la lista  //ya esta 
+# 4. Promedio
+# 5. Moda: es el valor que más se repite de un conjunto de datos
+# 6. Rango: es la diferencia entre el valor máximo y el valor mínimo de un
+# conjuto de datos}
+
+# tupla = ()
+# while True:
+#     try:
+#         numero = int(input("Ingrese un número: "))
+#         # Crear una nueva tupla que contiene los elementos originales más el nuevo número
+#         tupla += (numero,)
+#     except ValueError:
+   
+#         break
+
+# print("Tupla resultante:", tupla)
+
+lista= []
+numeros= int(input("ingrese los numeros que necesite: "))
+lista.append(numeros)
+print(lista)
