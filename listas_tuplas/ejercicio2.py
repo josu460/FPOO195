c95b0ab52d311ecc66f2b92cbebfbd5fba82d3ff
Josué Adrián Rivera Cruz <145291448+josu460@users.noreply.github.com>
2024-02-12 23:28:42 -0600

    ejercicios terminados

--- listas_tuplas/ejercicio2.py
+++ listas_tuplas/ejercicio2.py
@@ -4,5 +4,41 @@
 # b. Eliminar numero repetidos
 # c. Remplazar los repetidos con 0
 
+
+
 import random
-print(random.randrange(10,20))
+
+def contar_repetidos(lista):
+    repetidos = []
+    for i in lista:
+        if lista.count(i) > 1 and i not in repetidos:
+            repetidos.append(i)
+    return repetidos
+
+def reemplazar_repetidos(lista):
+    for i in range(len(lista)):
+        if lista[i] in repetidos:
+            lista[i] = 0
+
+def eliminar_repetidos(lista):
+    for elemento in repetidos:
+        while elemento in lista:
+            lista.remove(elemento)
+    return lista
+
+aleatorios = [random.randint(10, 20) for _ in range(30)]
+print(aleatorios)
+
+seleccion = input("Introduce la letra de la opción que quieras realizar (a, b, c): ")
+
+repetidos = contar_repetidos(aleatorios)
+
+if seleccion == "a":
+    print(f'los valores repetidos son : {contar_repetidos(aleatorios)}')
+elif seleccion == "b":
+    print(f'los que no se eliminaron fueron:{eliminar_repetidos(aleatorios.copy())}')
+elif seleccion == "c":
+    reemplazar_repetidos(aleatorios)
+    print(f'los valores remplazados quedaron asi: {aleatorios}')
+else:
+    print("Introduce una de las tres letras, por favor.")
--- listas_tuplas/listas.py
+++ listas_tuplas/listas.py
@@ -13,19 +13,57 @@
 # 6. Rango: es la diferencia entre el valor máximo y el valor mínimo de un
 # conjuto de datos}
 
-# tupla = ()
-# while True:
-#     try:
-#         numero = int(input("Ingrese un número: "))
-#         # Crear una nueva tupla que contiene los elementos originales más el nuevo número
-#         tupla += (numero,)
-#     except ValueError:
-   
-#         break
 
-# print("Tupla resultante:", tupla)
+numeros = input("Ingrese los números que necesite separados por comas: ")
 
-lista= []
-numeros= int(input("ingrese los numeros que necesite: "))
-lista.append(numeros)
-print(lista)
+def agregar_numeros(numeros):
+    numeros_separados = numeros.split(",")
+    lista = []
+    for numero in numeros_separados:
+        lista.append(int(numero))
+    return lista
+
+lista = agregar_numeros(numeros)
+
+tupla = tuple(lista)
+
+
+def sumatoria(tupla):
+    return sum(tupla)
+
+def numero_mayor(tupla):
+    return max(tupla)
+
+def numero_menor(tupla):
+    return min(tupla)
+
+def promedio(tupla):
+    return sum(tupla)/len(tupla)
+
+def moda(tupla):
+    return max(set(tupla), key=tupla.count)
+
+def rango(tupla):
+    return max(tupla) - min(tupla)
+
+
+
+
+
+
+opcion = int(input("ingrese la opcion que desee: (numero del 1 al 6):  "))
+
+if opcion == 1:
+    print(sumatoria(tupla))
+elif opcion == 2:
+    print(numero_mayor(tupla))
+elif opcion == 3:
+    print(numero_menor(tupla))
+elif opcion == 4:
+    print(promedio(tupla))
+elif opcion == 5:
+    print(moda(tupla))
+elif opcion == 6:
+    print(rango(tupla))
+else:
+    print("Introduce un numero valido ")
