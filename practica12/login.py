bb1e5c40e0c437e457668bec52c9579d2cb9f3c6
Josué Adrián Rivera Cruz <145291448+josu460@users.noreply.github.com>
2024-03-06 23:45:51 -0600

    ya puede arreglar el bienvendio siempr

--- practica12/login.py
+++ practica12/login.py
@@ -2,19 +2,20 @@ from tkinter import *
 from tkinter import ttk, messagebox
 from practica import Usuario
 
-def validar_usuario(correo, contraseña):
-    if not correo or not contraseña: 
-        messagebox.showerror('Error', 'Por favor, complete todos los campos.')
-        return
 
-    usuario_valido = Usuario.validar_usuario(correo, contraseña)  
-    if usuario_valido:
-        messagebox.showinfo('Bienvenido', '¡Bienvenido!')
+def validar_usuario(input1, input2):
+    correo = input1.get()
+    contraseña = input2.get()
+    if correo == "" or contraseña == "":
+        messagebox.showinfo('Error', 'Por favor, introduce tu correo y contraseña')
     else:
-        messagebox.showerror('Acceso denegado', '¡Acceso denegado!')
+        if Usuario.validar_usuario(correo, contraseña):
+            messagebox.showinfo('Bienvenido', 'Bienvenido!!')
+        else:
+            messagebox.showinfo('Acceso denegado', 'Acceso denegado!!')
 
 ventana = Tk()
-ventana.title("Login")
+ventana.title("login")
 ventana.geometry("600x500")
 
 seccion = Frame(ventana, bg="gray")
@@ -23,18 +24,17 @@ seccion.pack(expand=True, fill="both")
 titulo = Label(seccion, text="LOGIN", font=("Arial", 20))
 titulo.place(x=250, y=10)
 
-caja1 = ttk.Entry(seccion, text="black")
+caja1 = ttk.Entry(seccion)
 caja1.place(x=230, y=70, width=100, height=30)
 
 seccion2 = Frame(ventana, bg="gray")
 seccion2.pack(expand=True, fill="both")
 
-input2 = ttk.Entry(seccion2, text="black", show="*")  
+input2 = ttk.Entry(seccion2, show="*")
 input2.place(x=230, y=40, width=100, height=30)
 
-botonEnviar = Button(seccion2, text="LOGIN", fg="black", bg="white", command=lambda: validar_usuario(caja1.get(), input2.get()))
+botonEnviar = Button(seccion2, text="LOGIN", fg="black", bg="white", command=lambda: validar_usuario(caja1, input2))
 botonEnviar.configure(height=2, width=10)
 botonEnviar.place(x=230, y=100, width=100, height=30)
 
 ventana.mainloop()
-
--- practica12/practica.py
+++ practica12/practica.py
@@ -1,5 +1,5 @@
 class Usuario:
-    _lista_usuarios = []
+    listausuarios = []
 
     def __init__(self, id, nombre, apellido_P, apellido_M, correo, contraseña):
         self.__id = id
@@ -27,11 +27,29 @@ class Usuario:
     def getcontraseña(self):
         return self.__contraseña
 
-    def agregar_usuario(self):
-        Usuario._lista_usuarios.append(self)
+    def setid(self, id):
+        self.__id = id
 
-    def consultar_usuarios(self):
-        for usuario in Usuario._lista_usuarios:
+    def setnombre(self, nombre):
+        self.__nombre = nombre
+
+    def setapellido_P(self, apellido_P):
+        self.__apellido_P = apellido_P
+
+    def setapellido_M(self, apellido_M):
+        self.__apellido_M = apellido_M
+
+    def setcorreo(self, correo):
+        self.__correo = correo
+
+    def setcontraseña(self, contraseña):
+        self.__contraseña = contraseña
+
+    def agregar_usuario(self, usuario):
+        self.listausuarios.append(usuario)
+
+    def consultar_usuario(self):
+        for usuario in self.listausuarios:
             print("ID: ", usuario.getid())
             print("Nombre: ", usuario.getnombre())
             print("Apellido Paterno: ", usuario.getapellido_P())
@@ -40,27 +58,25 @@ class Usuario:
             print("Contraseña: ", usuario.getcontraseña())
 
     def eliminar_usuario(self, id):
-        for usuario in Usuario._lista_usuarios:
+        for usuario in self.listausuarios:
             if usuario.getid() == id:
-                Usuario._lista_usuarios.remove(usuario)
+                self.listausuarios.remove(usuario)
                 break
 
     def modificar_usuario(self, id, nombre, apellidoP, apellidoM, correo, contraseña):
-        for usuario in Usuario._lista_usuarios:
+        for usuario in self.listausuarios:
             if usuario.getid() == id:
-                usuario.__nombre = nombre
-                usuario.__apellido_P = apellidoP
-                usuario.__apellido_M = apellidoM
-                usuario.__correo = correo
-                usuario.__contraseña = contraseña
-                break
-
-    def validar_usuario(self, correo, contraseña):
-        for usuario in Usuario._lista_usuarios:
+                usuario.setnombre(nombre)
+                usuario.setapellido_P(apellidoP)
+                usuario.setapellido_M(apellidoM)
+                usuario.setcorreo(correo)
+                usuario.setcontraseña(contraseña)
+    @classmethod
+    def validar_usuario(cls, correo, contraseña):
+        for usuario in cls.listausuarios:
             if usuario.getcorreo() == correo and usuario.getcontraseña() == contraseña:
                 return True
         return False
-
 
 opcion = ' '
 while opcion != 'X':
@@ -82,16 +98,20 @@ while opcion != 'X':
         correo = input("Introduce el correo del usuario: ")
         contraseña = input("Introduce la contraseña del usuario: ")
         usuarionuevo = Usuario(id, nombre, apellido_P, apellido_M, correo, contraseña)
-        usuarionuevo.agregar_usuario()
+        usuarionuevo.agregar_usuario(usuarionuevo)
         print("Usuario creado")
     elif opcion == 'C':
         print("------------Lista de usuarios------------")
-        usuarionuevo = Usuario(None, None, None, None, None, None)
-        usuarionuevo.consultar_usuarios()
+        for usuario in Usuario.listausuarios:
+            print("ID: ", usuario.getid())
+            print("Nombre: ", usuario.getnombre())
+            print("Apellido Paterno: ", usuario.getapellido_P())
+            print("Apellido Materno: ", usuario.getapellido_M())
+            print("Correo: ", usuario.getcorreo())
+            print("Contraseña: ", usuario.getcontraseña())
     elif opcion == 'E':
         id_eliminar = input("Introduce el ID del usuario que deseas eliminar: ")
-        usuarionuevo = Usuario(None, None, None, None, None, None)
-        usuarionuevo.eliminar_usuario(id_eliminar)
+        Usuario.eliminar_usuario(id_eliminar)
         print("El usuario ha sido eliminado")
     elif opcion == 'M':
         id_modificar = input("Ingrese el ID del usuario que quieres modificar: ")
@@ -100,17 +120,16 @@ while opcion != 'X':
         apellidoM = input("Ingrese el nuevo apellido materno: ")
         correo = input("Ingrese el nuevo correo: ")
         contraseña = input("Ingrese la nueva contraseña: ")
-        usuarionuevo = Usuario(None, None, None, None, None, None)
-        usuarionuevo.modificar_usuario(id_modificar, nombre, apellidoP, apellidoM, correo, contraseña)
+        Usuario.modificar_usuario(id_modificar, nombre, apellidoP, apellidoM, correo, contraseña)
         print("El usuario ha sido modificado")
     elif opcion == 'I':
         print("------------LOGIN------------")
         correo_login = input("Ingrese su correo: ")
         contraseña_login = input("Ingrese su contraseña: ")
-        usuarionuevo = Usuario(None, None, None, None, None, None)
-        if usuarionuevo.validar_usuario(correo_login, contraseña_login):
+        if Usuario.validar_usuario(correo_login, contraseña_login):
             print("Bienvenido")
         else:
             print("Acceso denegado")
+
     else:
         print("Opción no válida")
