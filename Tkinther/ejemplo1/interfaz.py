ca24406895d79218133b303371d980dc95888292
Josué Adrián Rivera Cruz <145291448+josu460@users.noreply.github.com>
2024-03-10 22:27:44 -0600

    ejercicio series

--- Tkinther/ejemplo1/interfaz.py
+++ Tkinther/ejemplo1/interfaz.py
@@ -14,6 +14,21 @@ def click_agregar_lista():
     global lista
     resultado = mi_serie.agregar_a_lista(lista)
     messagebox.showinfo('La serie se agregó a la lista', resultado)
+    
+def click_marcar_como_completada():
+    mi_serie.marcar_como_completada()
+    messagebox.showinfo('La serie se completó', f"{mi_serie.titulo} se ha marcado como completada")
+    
+
+def click_ver_lista():
+    text_widget.delete('1.0', tk.END)
+    for serie in lista:
+        text_widget.insert(tk.END, serie.detalles() + "\n")
+        
+def click_calificar():
+    calificacion = entry2.get()
+    resultado = mi_serie.calificar(calificacion)
+    messagebox.showinfo('Calificación', resultado)
 
 # Crear la ventana principal
 root = tk.Tk()
@@ -24,20 +39,33 @@ label = tk.Label(root, text="¡Qué serie deseas ver!")
 label.pack()
 
 # Botón para reproducir la serie
-button = tk.Button(root, text="Reproducir", command=reproducir_serie)
-button.pack()
+button = tk.Button(root, text="Reproducir", command=reproducir_serie, bg='blue', fg='white')
+button.pack(pady=10)
 
 # Botón para agregar a la lista
-button2 = tk.Button(root, text="Agregar a la lista", command=click_agregar_lista)
-button2.pack()
+button2 = tk.Button(root, text="Agregar a la lista", command=click_agregar_lista, bg='green', fg='white')
+button2.pack(pady=10)
+
+button4 = tk.Button(root, text="Marcar como completada", command=click_marcar_como_completada, bg='red', fg='white')
+button4.pack(pady=10)
+
+entry2 = tk.Entry(root)
+entry2.pack(pady=10)
+
+button5 = tk.Button(root, text="Calificar", command=click_calificar, bg='yellow', fg='black')
+button5.pack(pady=10)
+
+
+button3 = tk.Button(root, text="Ver lista", command=click_ver_lista, bg='purple', fg='white')
+button3.pack(pady=10)
 
 # Cuadro de entrada
-entry = tk.Entry(root)
-entry.pack()
+entry = tk.Entry(root, width=50 )
+entry.pack(pady=10)
 
 # Cuadro de texto
 text_widget = tk.Text(root)
-text_widget.pack()
+text_widget.pack(pady=10)
 
 # Crear una lista para almacenar las series
 lista = []
--- Tkinther/ejemplo1/main.py
+++ Tkinther/ejemplo1/main.py
@@ -1,24 +1,11 @@
 class Serie:
-    def __init__(self, titulo,genero,calificacion =None,status='sin reproducir', horasestimadas=10):
+    def __init__(self, titulo, genero, calificacion=None, status='sin reproducir', horasestimadas=10):
         self.titulo = titulo
         self.genero = genero
         self.calificacion = calificacion
         self.status = status
         self.horasestimadas = horasestimadas
-    
-    
-    # def gettitulo(self):
-    #     return self.titulo
-    # def getgenero(self):
-    #     return self.genero
-    # def getcalificacion(self):
-    #     return self.calificacion
-    # def getstatus(self):
-    #     return self.status
-    # def gethorasestimadas(self):
-    #     return self.horasestimadas
-    
-      
+
     def reproducir(self):
         return f"Reproduciendo {self.titulo}"
 
@@ -30,11 +17,16 @@ class Serie:
         self.status = "Completada"
 
     def calificar(self, calificacion):
-        self.__calificacion = calificacion
+        self.calificacion = calificacion
         return f"Se ha calificado {self.titulo} con {calificacion} estrellas"
 
     def ver_series(self, lista):
         for serie in lista:
             print(f"{serie.titulo} - {serie.genero} - {serie.calificacion} - {serie.status} - {serie.horasestimadas}")
-    
+
+    def detalles(self):
+        return f"{self.titulo} - {self.genero} - {self.calificacion} - {self.status} - {self.horasestimadas}"
+
 lista = []
+
+
